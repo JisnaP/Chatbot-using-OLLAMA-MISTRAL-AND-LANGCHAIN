6704446c353ec3b830b8224d7f1d419bb9dfff91
JisnaP <jisna12@gmail.com>
2025-02-10 10:01:30 +0300

    changes updated

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.llms import Ollama
-import streamlit as st
 import os
 from dotenv import load_dotenv
+import streamlit as st
+from langsmith import Client
+from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
 load_dotenv()
-os.environ["LANGSMITH_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-LANGSMITH_TRACING= "true"
+
+
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGSMITH_API_KEY")  
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_PROJECT"] = "Chatbot-Session"  
+
+
+client = Client()
+
 
 st.title("CHATBOT USING MISTRAL AND LANGCHAIN")
-input=st.text_input("Type your queries here")
-user_input=input
-prompt=ChatPromptTemplate.from_messages(
-    [
+user_input = st.text_input("Type your queries here")
+
+
+prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful AI bot. Please respond to the user queries."),
-    ("user", "Question:{question}")
-    
-    ]
+    ("user", "Question: {question}")
+])
 
-)
 
-llm=Ollama(model="mistral")
-outparser=StrOutputParser()
-chain=prompt|llm|outparser
-if input:
-    response=chain.invoke({"question":input})
-    st.write(response)
+llm = Ollama(model="mistral")
+outparser = StrOutputParser()
+chain = prompt | llm | outparser
+
+
+if user_input:
+    try:
+        response = chain.invoke({"question": user_input})  
+        st.write(response)  
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
